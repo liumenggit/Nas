f56709e1018af4ce1b5b212a95c4a7c66826bdfa
liumenggit <rrsyycm@163.com>
2020-03-26 14:30:31 +0000

    首次发布

--- nas.js
+++ nas.js
@@ -1,17 +1,68 @@
+Jsonrpc = 'http://10.10.10.10:6800/jsonrpc'; //Aria2jsonrpc地址
+JsonrpcKey = 'rrsyycm'; //Aria2jsonrpc密钥
+SecretKey = '此处填写通讯密钥'; //与捷径的通讯密钥
+PhantomjsPort = 4363; //Phantomjs与捷径的通讯端口
+Functions = {
+  "电影": {
+    "Select": "Video",
+    "Down": "VideoDown",
+    "Dir": "\/video",
+    "Msg": "请输入影视名称"
+  },
+  "音乐": {
+    "Select": "Music",
+    "Down": "MusicDown",
+    "Dir": "\/music",
+    "Msg": "请输入音乐名"
+  },
+  "P站": {
+    "Select": "PornHub",
+    "Down": "PornHubDown",
+    "Dir": "\/pornhub"
+  },
+  "下载": {
+    "Select": "Down",
+    "Dir": "\/data",
+    "Msg": "请输入链接地址"
+  },
+  "使用指南": {
+    "Select": "Guide",
+    "Msg": "http:\/\/wan7.xin\/nasjiejing.html"
+  },
+  "退出服务端": {
+    "Select": "Quit",
+    "Msg": "退出Phantomjs服务，退出后将无法使用此捷径。"
+  }
+}; //方法配置
+//Phantomjs
 var server = require('webserver').create();
 var page = require('webpage').create();
-console.log('-------------Start-------------');
-//优化配置
-var service = server.listen(4363, function(request, response) {
+console.log('初始化完毕');
+var service = server.listen(PhantomjsPort, function(request, response) {
   KjPost = JSON.parse(request.post);
   console.log('--------------------------Start--------------------------')
   var StartTime = new Date();
   console.log(StartTime.toJSON(), KjPost['fun'], KjPost['url'])
-  Nas[KjPost.fun](request, response);
+  if (KjPost['secretkey'] != md5(SecretKey)) {
+    console.log('密钥错误:' + KjPost['secretkey']);
+    response.statusCode = 200;
+    response.write(JSON.stringify({
+      "info": "error",
+      "msg": "密钥错误"
+    }));
+    response.close();
+  } else {
+    Nas[KjPost.fun](request, response);
+  }
   console.log('---------------------------------------------------------')
 });
 
 var Nas = {
+  'Update': function(request, response) {
+    response.statusCode = 200;
+    response.write(JSON.stringify(Functions));
+    response.close();
+  },
   'Video': function(request, response) {
     //获取匹配的视频
     KjPost = JSON.parse(request.post)
@@ -133,7 +184,7 @@ var Nas = {
     response.close();
   },
   'Down': function(request, response) {
-    url = 'http://10.10.10.10:6800/jsonrpc'
+    var url = Jsonrpc;
     var DownInfo = JSON.parse(request.post)['url'];
     var DownItem = JSON.parse(DownInfo);
     var namedir = {};
@@ -142,7 +193,7 @@ var Nas = {
       jsonrpc: '2.0',
       method: 'aria2.addUri',
       params: [
-        "token:rrsyycm",
+        "token:" + JsonrpcKey + "",
         [DownItem.url]
       ]
     };
@@ -162,20 +213,11 @@ var Nas = {
       },
       data: JSON.stringify(json_rpc)
     };
+    console.log(JSON.stringify(settings));
     page.open(url, settings, function(status) {
-      // if (status !== 'success') {
-      //   console.log('Unable to post!');
-      //   response.statusCode = 200;
-      //   response.write(JSON.stringify({
-      //     "info": "error",
-      //     "msg": "无法访问:aria2" + status
-      //   }));
-      //   response.close();
-      // } else {
-      // }
       response.statusCode = 200;
       response.write(JSON.stringify({
-        "info": "error",
+        "info": "DownFun",
         "msg": "Down-ok:" + (DownItem.name ? DownItem.name : DownItem.url)
       }));
       // + DownItem.name ? DownItem.name : DownItem.url
@@ -186,7 +228,7 @@ var Nas = {
   'Quit': function(request, response) {
     response.statusCode = 200;
     response.write(JSON.stringify({
-      "info": "error",
+      "info": "ok",
       "msg": "已经退出服务端"
     }));
     response.close();
@@ -254,7 +296,7 @@ var Nas = {
         response.statusCode = 200;
         // response.write(MusicUrl[0]['url']);
         response.write(JSON.stringify({
-          "info": "ok",
+          "info": "Down",
           "Down": {
             "name": MusicName + '.' + MusicUrl[0]['url'].replace(/.+\./, ""),
             "url": MusicUrl[0]['url']
@@ -265,14 +307,14 @@ var Nas = {
     })
   }
 };
-
+//不加载css
 page.onResourceRequested = function(requestData, networkRequest) {
   var match = requestData.url.match(/\/\/.*.css/g);
   if (match != null) {
     networkRequest.cancel(); // or .abort()
   }
 };
-
+//url编码解码
 function urlEncode(str) {
   var ret = "";
   var strSpecial = "!\"#$%&’()*+,/:;<=>?[]^`{|}~%";
@@ -359,3 +401,263 @@ function AnsiToUnicode(chrCode) {
   }
   return parseInt(chrHex, 16)
 }
+//md5
+(function($) {
+  'use strict';
+
+  /*
+   * Add integers, wrapping at 2^32. This uses 16-bit operations internally
+   * to work around bugs in some JS interpreters.
+   */
+  function safe_add(x, y) {
+    var lsw = (x & 0xFFFF) + (y & 0xFFFF),
+      msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+    return (msw << 16) | (lsw & 0xFFFF);
+  }
+
+  /*
+   * Bitwise rotate a 32-bit number to the left.
+   */
+  function bit_rol(num, cnt) {
+    return (num << cnt) | (num >>> (32 - cnt));
+  }
+
+  /*
+   * These functions implement the four basic operations the algorithm uses.
+   */
+  function md5_cmn(q, a, b, x, s, t) {
+    return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s), b);
+  }
+
+  function md5_ff(a, b, c, d, x, s, t) {
+    return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t);
+  }
+
+  function md5_gg(a, b, c, d, x, s, t) {
+    return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t);
+  }
+
+  function md5_hh(a, b, c, d, x, s, t) {
+    return md5_cmn(b ^ c ^ d, a, b, x, s, t);
+  }
+
+  function md5_ii(a, b, c, d, x, s, t) {
+    return md5_cmn(c ^ (b | (~d)), a, b, x, s, t);
+  }
+
+  /*
+   * Calculate the MD5 of an array of little-endian words, and a bit length.
+   */
+  function binl_md5(x, len) {
+    /* append padding */
+    x[len >> 5] |= 0x80 << (len % 32);
+    x[(((len + 64) >>> 9) << 4) + 14] = len;
+
+    var i, olda, oldb, oldc, oldd,
+      a = 1732584193,
+      b = -271733879,
+      c = -1732584194,
+      d = 271733878;
+
+    for (i = 0; i < x.length; i += 16) {
+      olda = a;
+      oldb = b;
+      oldc = c;
+      oldd = d;
+
+      a = md5_ff(a, b, c, d, x[i], 7, -680876936);
+      d = md5_ff(d, a, b, c, x[i + 1], 12, -389564586);
+      c = md5_ff(c, d, a, b, x[i + 2], 17, 606105819);
+      b = md5_ff(b, c, d, a, x[i + 3], 22, -1044525330);
+      a = md5_ff(a, b, c, d, x[i + 4], 7, -176418897);
+      d = md5_ff(d, a, b, c, x[i + 5], 12, 1200080426);
+      c = md5_ff(c, d, a, b, x[i + 6], 17, -1473231341);
+      b = md5_ff(b, c, d, a, x[i + 7], 22, -45705983);
+      a = md5_ff(a, b, c, d, x[i + 8], 7, 1770035416);
+      d = md5_ff(d, a, b, c, x[i + 9], 12, -1958414417);
+      c = md5_ff(c, d, a, b, x[i + 10], 17, -42063);
+      b = md5_ff(b, c, d, a, x[i + 11], 22, -1990404162);
+      a = md5_ff(a, b, c, d, x[i + 12], 7, 1804603682);
+      d = md5_ff(d, a, b, c, x[i + 13], 12, -40341101);
+      c = md5_ff(c, d, a, b, x[i + 14], 17, -1502002290);
+      b = md5_ff(b, c, d, a, x[i + 15], 22, 1236535329);
+
+      a = md5_gg(a, b, c, d, x[i + 1], 5, -165796510);
+      d = md5_gg(d, a, b, c, x[i + 6], 9, -1069501632);
+      c = md5_gg(c, d, a, b, x[i + 11], 14, 643717713);
+      b = md5_gg(b, c, d, a, x[i], 20, -373897302);
+      a = md5_gg(a, b, c, d, x[i + 5], 5, -701558691);
+      d = md5_gg(d, a, b, c, x[i + 10], 9, 38016083);
+      c = md5_gg(c, d, a, b, x[i + 15], 14, -660478335);
+      b = md5_gg(b, c, d, a, x[i + 4], 20, -405537848);
+      a = md5_gg(a, b, c, d, x[i + 9], 5, 568446438);
+      d = md5_gg(d, a, b, c, x[i + 14], 9, -1019803690);
+      c = md5_gg(c, d, a, b, x[i + 3], 14, -187363961);
+      b = md5_gg(b, c, d, a, x[i + 8], 20, 1163531501);
+      a = md5_gg(a, b, c, d, x[i + 13], 5, -1444681467);
+      d = md5_gg(d, a, b, c, x[i + 2], 9, -51403784);
+      c = md5_gg(c, d, a, b, x[i + 7], 14, 1735328473);
+      b = md5_gg(b, c, d, a, x[i + 12], 20, -1926607734);
+
+      a = md5_hh(a, b, c, d, x[i + 5], 4, -378558);
+      d = md5_hh(d, a, b, c, x[i + 8], 11, -2022574463);
+      c = md5_hh(c, d, a, b, x[i + 11], 16, 1839030562);
+      b = md5_hh(b, c, d, a, x[i + 14], 23, -35309556);
+      a = md5_hh(a, b, c, d, x[i + 1], 4, -1530992060);
+      d = md5_hh(d, a, b, c, x[i + 4], 11, 1272893353);
+      c = md5_hh(c, d, a, b, x[i + 7], 16, -155497632);
+      b = md5_hh(b, c, d, a, x[i + 10], 23, -1094730640);
+      a = md5_hh(a, b, c, d, x[i + 13], 4, 681279174);
+      d = md5_hh(d, a, b, c, x[i], 11, -358537222);
+      c = md5_hh(c, d, a, b, x[i + 3], 16, -722521979);
+      b = md5_hh(b, c, d, a, x[i + 6], 23, 76029189);
+      a = md5_hh(a, b, c, d, x[i + 9], 4, -640364487);
+      d = md5_hh(d, a, b, c, x[i + 12], 11, -421815835);
+      c = md5_hh(c, d, a, b, x[i + 15], 16, 530742520);
+      b = md5_hh(b, c, d, a, x[i + 2], 23, -995338651);
+
+      a = md5_ii(a, b, c, d, x[i], 6, -198630844);
+      d = md5_ii(d, a, b, c, x[i + 7], 10, 1126891415);
+      c = md5_ii(c, d, a, b, x[i + 14], 15, -1416354905);
+      b = md5_ii(b, c, d, a, x[i + 5], 21, -57434055);
+      a = md5_ii(a, b, c, d, x[i + 12], 6, 1700485571);
+      d = md5_ii(d, a, b, c, x[i + 3], 10, -1894986606);
+      c = md5_ii(c, d, a, b, x[i + 10], 15, -1051523);
+      b = md5_ii(b, c, d, a, x[i + 1], 21, -2054922799);
+      a = md5_ii(a, b, c, d, x[i + 8], 6, 1873313359);
+      d = md5_ii(d, a, b, c, x[i + 15], 10, -30611744);
+      c = md5_ii(c, d, a, b, x[i + 6], 15, -1560198380);
+      b = md5_ii(b, c, d, a, x[i + 13], 21, 1309151649);
+      a = md5_ii(a, b, c, d, x[i + 4], 6, -145523070);
+      d = md5_ii(d, a, b, c, x[i + 11], 10, -1120210379);
+      c = md5_ii(c, d, a, b, x[i + 2], 15, 718787259);
+      b = md5_ii(b, c, d, a, x[i + 9], 21, -343485551);
+
+      a = safe_add(a, olda);
+      b = safe_add(b, oldb);
+      c = safe_add(c, oldc);
+      d = safe_add(d, oldd);
+    }
+    return [a, b, c, d];
+  }
+
+  /*
+   * Convert an array of little-endian words to a string
+   */
+  function binl2rstr(input) {
+    var i,
+      output = '';
+    for (i = 0; i < input.length * 32; i += 8) {
+      output += String.fromCharCode((input[i >> 5] >>> (i % 32)) & 0xFF);
+    }
+    return output;
+  }
+
+  /*
+   * Convert a raw string to an array of little-endian words
+   * Characters >255 have their high-byte silently ignored.
+   */
+  function rstr2binl(input) {
+    var i,
+      output = [];
+    output[(input.length >> 2) - 1] = undefined;
+    for (i = 0; i < output.length; i += 1) {
+      output[i] = 0;
+    }
+    for (i = 0; i < input.length * 8; i += 8) {
+      output[i >> 5] |= (input.charCodeAt(i / 8) & 0xFF) << (i % 32);
+    }
+    return output;
+  }
+
+  /*
+   * Calculate the MD5 of a raw string
+   */
+  function rstr_md5(s) {
+    return binl2rstr(binl_md5(rstr2binl(s), s.length * 8));
+  }
+
+  /*
+   * Calculate the HMAC-MD5, of a key and some data (raw strings)
+   */
+  function rstr_hmac_md5(key, data) {
+    var i,
+      bkey = rstr2binl(key),
+      ipad = [],
+      opad = [],
+      hash;
+    ipad[15] = opad[15] = undefined;
+    if (bkey.length > 16) {
+      bkey = binl_md5(bkey, key.length * 8);
+    }
+    for (i = 0; i < 16; i += 1) {
+      ipad[i] = bkey[i] ^ 0x36363636;
+      opad[i] = bkey[i] ^ 0x5C5C5C5C;
+    }
+    hash = binl_md5(ipad.concat(rstr2binl(data)), 512 + data.length * 8);
+    return binl2rstr(binl_md5(opad.concat(hash), 512 + 128));
+  }
+
+  /*
+   * Convert a raw string to a hex string
+   */
+  function rstr2hex(input) {
+    var hex_tab = '0123456789abcdef',
+      output = '',
+      x,
+      i;
+    for (i = 0; i < input.length; i += 1) {
+      x = input.charCodeAt(i);
+      output += hex_tab.charAt((x >>> 4) & 0x0F) +
+        hex_tab.charAt(x & 0x0F);
+    }
+    return output;
+  }
+
+  /*
+   * Encode a string as utf-8
+   */
+  function str2rstr_utf8(input) {
+    return unescape(encodeURIComponent(input));
+  }
+
+  /*
+   * Take string arguments and return either raw or hex encoded strings
+   */
+  function raw_md5(s) {
+    return rstr_md5(str2rstr_utf8(s));
+  }
+
+  function hex_md5(s) {
+    return rstr2hex(raw_md5(s));
+  }
+
+  function raw_hmac_md5(k, d) {
+    return rstr_hmac_md5(str2rstr_utf8(k), str2rstr_utf8(d));
+  }
+
+  function hex_hmac_md5(k, d) {
+    return rstr2hex(raw_hmac_md5(k, d));
+  }
+
+  function md5(string, key, raw) {
+    if (!key) {
+      if (!raw) {
+        return hex_md5(string);
+      }
+      return raw_md5(string);
+    }
+    if (!raw) {
+      return hex_hmac_md5(key, string);
+    }
+    return raw_hmac_md5(key, string);
+  }
+
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return md5;
+    });
+  } else {
+    $.md5 = md5;
+  }
+}(this));
